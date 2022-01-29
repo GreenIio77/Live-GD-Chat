43f5b3052f5e5166fa38eb75aefe0ac966d00f3d
Greenlio <94572650+GreenIio77@users.noreply.github.com>
2022-01-29 09:44:21 -0600

    Create Util.cpp

---
+++ Util.cpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <functional>
+#include <thread>
+class Util
+{
+public:
+	static void timer_start(std::function<void(void)> func, unsigned int interval)
+	{
+		std::thread([func, interval]() {
+			while (true)
+			{
+				func();
+				std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			}
+		}).detach();
+	}
+};
